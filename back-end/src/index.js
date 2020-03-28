87cdaee3f0bd182dbf83fdadc926a6ada16ea348
Boa Vista Servi;os S/A <eu@feabreu.com.br>
2020-03-28 01:26:25 -0300

    criando o back-end

---
+++ back-end/src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors'); 
+const routes = require('./routes'); 
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+// Metodo get recebe/lista informacao
+// Metodo POST criar informacoao no bakc
+// PUT alterar informacao do back
+// DELETE deletar uma informacao do back
+
+// Tipos de parametro
+
+// Query params: parametros nomeados enviados na rota apos o ? e geralmente servem para filtros, paginacao.
+// Route Params: parametros utilizados para identificar recursos. (ID)
+// Request Body: Corpo da requisicao utilizado para criar ou alterar recursos
+
+
+// banco de dados sql -> SQLite
+// NoSql -> mongodb
+
+// Driver: SELECT * FROM USERS
+// Query Builder: table("users").select("*").where("")
+
+app.listen(3333);
