b2501560e74a6e202ffdd0f2a8317b8149c83da0
Boa Vista Servi;os S/A <eu@feabreu.com.br>
2020-03-31 19:33:52 -0300

    mobile do app

---
+++ mobile/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Routes from './src/routes';
+
+// view - div,section,aside
+// text- p,h1,span,strong
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
